681c3324b187f0825855471836dc4e90344a7457
Gaurav Viramgami <58519896+Gaurav7214@users.noreply.github.com>
2021-02-11 21:43:44 +0530

    1st

--- LED_Blink_NodeMCU/LED_Blink_NodeMCU.ino
+++ LED_Blink_NodeMCU/LED_Blink_NodeMCU.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);
+// 2 - LED_BUILTIN
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
 }
